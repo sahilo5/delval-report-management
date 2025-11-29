85ac2e36df41b6d8c56d222a046dafa3aabcc147
sahilo5 <sahil.work.0101@gmail.com>
2025-11-29 14:10:58 +0530

    Added Csrf origin to settings

--- ReportManagement/settings.py
+++ ReportManagement/settings.py
@@ -99,6 +99,11 @@ DATABASES = {
     }
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://delval-report-management-production.up.railway.app",
+]
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
