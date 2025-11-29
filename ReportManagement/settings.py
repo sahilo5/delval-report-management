83b5293a4117d1ab7a095ab85efd22f95bf15711
sahilo5 <sahil.work.0101@gmail.com>
2025-11-29 13:59:10 +0530

    Added host

--- ReportManagement/settings.py
+++ ReportManagement/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = [
-    'report-management-t9qm.onrender.com',
+    'delval-report-management-production.up.railway.app',
     'localhost',
     '127.0.0.1'
 ]
